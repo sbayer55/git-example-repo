878226724a79c25310d4a41a75611854c1106398
sbayer55 <sbayer55@gmail.com>
2017-11-29 09:45:08 -0600

    Add starting number

--- awesome_project.py
+++ awesome_project.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-def count(target):
-    for i in range(0, target):
+def count(start, target):
+    for i in range(start, target):
         print(str(i))
 
 if __name__ == "__main__":
     print("I am an awesome program!")
-    count(10)
+    count(1, 10)
