c3a7a0e0547224a334cc9e9838697eb54fc1bc42
sbayer55 <sbayer55@gmail.com>
2017-11-29 09:39:03 -0600

    Count to 10

---
+++ awesome_project.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+def count(target):
+    for i in range(0, target):
+        print(str(i))
+
+if __name__ == "__main__":
+    print("I am an awesome program!")
+    count(10)
